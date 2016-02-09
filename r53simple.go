4c42beaac7987b88cbbc316aa6c6c237e39d1e0e
dbnegative <shortjay@gmail.com>
2016-02-09 15:50:59 +0000

    Added basic functionality and support for record types that accept IP address's as values.

--- r53simple.go
+++ r53simple.go
@@ -13,8 +13,8 @@ import (
 //Recordset contains Route53 Recordset
 type Recordset struct {
 	Record       *route53.ResourceRecordSet
-	Parameters   *route53.ChangeResourceRecordSetsInput
 	HostZoneName string
+	HostZoneID   string
 }
 
 //Not every ResourceRecordSet memember is supported yet
@@ -22,18 +22,9 @@ var hostedzonenameFlag string
 var hostnameFlag string
 var recordtypeFlag string
 var ipaddressFlag string
-
-//var regionFlag string
-//var geolocationFlag string
-//var healthcheckidFlag string
-//var failoverFlag string
-//var resourceidentifierFlag string
 var ttlFlag int64
 
-//var trafficpolicyinstanceidFlag string
-//var aliastargetFlag string
-//var weightFlag int64
-
+//init all flag values
 func init() {
 	flag.StringVar(&hostedzonenameFlag, "hostedzone", "", "The hosted zone name that will be updated (Required)")
 	flag.StringVar(&hostnameFlag, "recordname", "", "The name of the FQDN you want to perform the action on (Required)")
@@ -47,37 +38,48 @@ func main() {
 
 	arglen := len(os.Args)
 	record := &Recordset{}
+	record.Record = &route53.ResourceRecordSet{}
+	record.Record.ResourceRecords = make([]*route53.ResourceRecord, 1)
+
 	if arglen <= 1 {
 		fmt.Println("No option set try --help for more information")
 	}
 
 	if (hostedzonenameFlag != "") && (hostnameFlag != "") && (recordtypeFlag != "") {
-		//set the hosted zone name from the flag
+		//debug
+		fmt.Println("hosted name: ", hostedzonenameFlag, " recordname: ", hostnameFlag, " recordtype: ", recordtypeFlag)
+		// TODO: Validate FLAGS!!!
+		//set the vars from flags
 		record.HostZoneName = hostedzonenameFlag
+		record.Record.Name = aws.String(hostnameFlag)
 		record.Record.TTL = aws.Int64(ttlFlag)
 		record.Record.Type = aws.String(recordtypeFlag)
-		record.Record.ResourceRecord.Value = aws.String(ipaddressFlag)
+		record.Record.ResourceRecords[0] = &route53.ResourceRecord{Value: aws.String(ipaddressFlag)}
 		//get the hosted zone id from the name
 		record.getzoneid()
-
+		//upsert record
+		record.recordupsert()
 	}
 
 }
 
-func (record Recordset) getzoneid() {
+func (record *Recordset) getzoneid() {
 	svc := route53.New(session.New())
+	fmt.Println("looking for the record: ", record.HostZoneName)
+
 	params := &route53.ListHostedZonesByNameInput{
 		DNSName:  aws.String(record.HostZoneName),
 		MaxItems: aws.String("1"),
 	}
+
 	resp, err := svc.ListHostedZonesByName(params)
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(-1)
 	}
 	//if result does not match error out
-	if record.HostZoneName == *resp.DNSName {
-		record.Parameters.HostedZoneId = resp.HostedZoneId
+	if record.HostZoneName+"." == *resp.HostedZones[0].Name {
+		record.HostZoneID = *resp.HostedZones[0].Id
 	} else {
 		fmt.Println("Error: Could not find Hostzone: ", record.HostZoneName)
 		os.Exit(-1)
@@ -87,10 +89,28 @@ func (record Recordset) getzoneid() {
 }
 
 //update zone record
-func (record Recordset) recordupsert() {
+func (record *Recordset) recordupsert() {
 	svc := route53.New(session.New())
 
-	resp, err := svc.ChangeResourceRecordSets(record.Parameters)
+	//initialise upsert params
+	parameters := &route53.ChangeResourceRecordSetsInput{}
+	parameters.ChangeBatch = &route53.ChangeBatch{}
+	parameters.HostedZoneId = aws.String(record.HostZoneID)
+	parameters.ChangeBatch.Changes = make([]*route53.Change, 1)
+	parameters.ChangeBatch.Changes[0] = &route53.Change{
+		Action: aws.String("UPSERT"),
+		ResourceRecordSet: &route53.ResourceRecordSet{
+			Type: aws.String(*record.Record.Type),
+			TTL:  aws.Int64(*record.Record.TTL),
+			Name: aws.String(*record.Record.Name),
+			ResourceRecords: []*route53.ResourceRecord{&route53.ResourceRecord{
+				Value: record.Record.ResourceRecords[0].Value,
+			}},
+		},
+	}
+
+	//fmt.Println(*record.Parameters.ChangeBatch.Changes[0].ResourceRecordSet)
+	resp, err := svc.ChangeResourceRecordSets(parameters)
 
 	if err != nil {
 		// Print the error, cast err to awserr.Error to get the Code and
